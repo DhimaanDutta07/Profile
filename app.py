877353708c41cbbd126efcd20abd0c7708f21616
DhimaanDutta07 <dhimaanmams.07@gmail.com>
2025-10-31 20:56:16 +0000

    files a

--- app.py
+++ app.py
@@ -14,3 +14,6 @@ def projects():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
